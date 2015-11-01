05e25691c1937e14d3378475e2142e56e59b3338
Cameron <cameron.p.buckingham@gmail.com>
2015-11-01 08:05:08 -0700

    constants

---
+++ constant.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  const pi float64 = 3.14159265
+
+var myName string = "Cameron"
+fmt.Println(len(myName))
+fmt.Println(myName + "is a \n robot")
+
+var isOver20 bool = true
+fmt.Println("%.3f \n, pi")
+fmt.Println("%T \n, pi")
+fmt.Println("%t \n, isOver20")
+fmt.Println("%d \n, 100")
+fmt.Println("%b \n, 100")
+fmt.Println("%c \n, 100")
+fmt.Println("%x \n, 100")
+fmt.Println("%d \n, 100")
+fmt.Println("%e \n, pi")
+
+}
