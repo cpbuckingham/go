24eaa98782f08cf10a495d14fecf9c94049c1744
Cameron <cameron.p.buckingham@gmail.com>
2015-11-22 13:24:29 -0700

    switch case

---
+++ swtich_case.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+
+  yourAge := 5
+
+switch yourAge{
+case 16: fmt.Println("go Drive")
+case 18: fmt.Println("go Vote")
+default : fmt.Println("go have fun")
+}
+}
