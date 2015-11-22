90bac9e6b6e110b8b62405dd7e9ef9c7898f2b2d
Cameron <cameron.p.buckingham@gmail.com>
2015-11-22 13:24:54 -0700

    variatic functions

---
+++ variatic_func.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+func main(){
+fmt.Println(subtractThem(1,2,3,4,5))
+}
+
+//func functionName (value receieveing) value return
+func subtractThem(args...int) int{
+  finalValue:=0
+  for _,value:= range args{
+    finalValue-=value
+  }
+  return
+}
